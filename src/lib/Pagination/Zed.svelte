b744e9d318878e80289e50d22b666fc833a02601
Scott Phillips <alan.scott.phillips@protonmail.ch>
2022-05-31 16:32:14 +0200

    Update Zed.svelte

--- src/lib/Pagination/Zed.svelte
+++ src/lib/Pagination/Zed.svelte
@@ -69,10 +69,14 @@
 	title="{title}" href="{cta}"
 	class="link">
 	<figure class="ma0 w-100 f6 mh0 ph3 ph3-ns ph1-m ph3-l pb4 pt5 cover shadow-5-hover transition-bs overflow-hidden"
-	style='background-position: 50% 0; background-image: linear-gradient( rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 0.50) 100%), url(https://viaggilevi.vercel.app/images/Tineye.Torres.del.Paine.National.Park.webp)'
+	style='background-position: 50% 0; background-image: linear-gradient( rgba(0, 0, 0, 0) 30%, rgba(0, 0, 0, 0.50) 100%), url({image})'
 	title={title}>
-	<code class="f8 bg-charcoal white">{image}</code>
+
 	<!-- debug: {JSON.stringify(image)} -->
+	<!-- https://viaggilevi.vercel.app/images/Tineye.Torres.del.Paine.National.Park.webp -->
+	<!-- <code class="f8 bg-charcoal white">{image}</code>
+	 -->
+
 	<!-- learn: take care of escaped [Object object] strings: stackoverflow.com/questions/25721164/how-to-fix-an-escaped-json-string-javascript#25721227 -->
 
 	<figurecap class="white ts1-dark-gray flex flex-column lh-solid">
